9419db8d7f16d2d158f00a0856687fed52ff3fd6
Semen Cernyshev <semen_ch@vk.com>
2023-04-05 12:28:25 +0300

    create token rollbar 5

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -11,11 +11,6 @@ import getRoutes from '../routes.js';
 import Header from './Header.jsx';
 import PrivateRoute from './PrivatePage.jsx';
 
-function TestError() {
-  const a = null;
-  return a.hello();
-}
-
 const App = () => (
   <div className="h-100">
     <div className="d-flex flex-column h-100">
@@ -33,7 +28,6 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
-      <TestError />
     </div>
     <ToastContainer
       position="top-right"
--- frontend/src/rollbar/rollbarConfig.js
+++ frontend/src/rollbar/rollbarConfig.js
@@ -1,6 +1,10 @@
 const rollbarConfig = {
-  accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
-  environment: 'testenv',
+  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+  payload: {
+    environment: 'production',
+  },
+  captureUncaught: true,
+  captureUnhandledRejections: true,
 };
 
 export default rollbarConfig;
